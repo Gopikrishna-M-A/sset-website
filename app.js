bc4f65d949715813e334fd1efee2f7f6b37dde70
gopikrishnan-L <125388612+gopikrishnan-L@users.noreply.github.com>
2023-04-05 00:21:30 +0530

    alumni pages partly added

--- app.js
+++ app.js
@@ -3,88 +3,97 @@ const ejs = require('ejs')
 const app = express()
 
 app.use(express.static(__dirname + '/public'));
-app.set('view engine', 'ejs')  
+app.set('view engine', 'ejs')
 
 
+// HOME ROUTE
+
+app.get("/", (req, res) => {
+    res.render('home/index')
+})
 
 
+// PLACEMENTS ROUTE
+
+app.get("/placements", (req, res) => {
+    res.render('placements/overview')
+})
+
+app.get("/placements/mous", (req, res) => {
+    res.render('placements/mous')
+})
+
+app.get("/placements/contact", (req, res) => {
+    res.render('placements/contact-us')
+})
+
+app.get("/placements/recruiters", (req, res) => {
+    res.render('placements/recruiters')
+})
+
+app.get("/placements/recruitment-process", (req, res) => {
+    res.render('placements/recruitment-process')
+})
 
 
-    // HOME ROUTE
+// DEPARTMENTS ROUTE 
 
-    app.get("/",(req,res)=>{
-        res.render('home/index')
-    })
+app.get("/departments", (req, res) => {
+    res.render('departments/cse@sset')
+})
 
-    
-    // PLACEMENTS ROUTE
+// CAMPUS ROUTE
 
-    app.get("/placements",(req,res)=>{
-        res.render('placements/overview')
-    })
-    
-    app.get("/placements/mous",(req,res)=>{
-        res.render('placements/mous')
-    })
+app.get("/campus", (req, res) => {
+    res.render('campus/overview')
+})
 
-    app.get("/placements/contact",(req,res)=>{
-        res.render('placements/contact-us')
-    })
+app.get("/campus/councelling", (req, res) => {
+    res.render('campus/councelling')
+})
 
-    app.get("/placements/recruiters",(req,res)=>{
-        res.render('placements/recruiters')
-    })
+app.get("/campus/facilities", (req, res) => {
+    res.render('campus/facilities')
+})
 
-    app.get("/placements/recruitment-process",(req,res)=>{
-        res.render('placements/recruitment-process')
-    })
+app.get("/campus/softskill", (req, res) => {
+    res.render('campus/softskill')
+})
 
+app.get("/campus/sewagetreatment", (req, res) => {
+    res.render('campus/sewagetreatment')
+})
 
-    // DEPARTMENTS ROUTE 
+app.get("/campus/systemofteaching", (req, res) => {
+    res.render('campus/systemofteach')
+})
 
-    app.get("/departments",(req,res)=>{
-        res.render('departments/cse@sset')
-    })
+// ACADEMICS ROUTE 
 
-    // CAMPUS ROUTE
-    
-    app.get("/campus",(req,res)=>{
-        res.render('campus/overview')
-    })
+app.get("/academics", (req, res) => {
+    res.render('academics/academics')
+})
 
-    app.get("/campus/councelling",(req,res)=>{
-        res.render('campus/councelling')
-    })
-
-    app.get("/campus/facilities",(req,res)=>{
-        res.render('campus/facilities')
-    })
-
-    app.get("/campus/softskill",(req,res)=>{
-        res.render('campus/softskill')
-    })
-
-    app.get("/campus/sewagetreatment",(req,res)=>{
-        res.render('campus/sewagetreatment')
-    })
-
-    app.get("/campus/systemofteaching",(req,res)=>{
-        res.render('campus/systemofteach')
-    })
-
-    // ACADEMICS ROUTE 
-
-    app.get("/academics",(req,res)=>{
-        res.render('academics/academics')
-    })
+//ALUMNI
+app.get("/alumni", (req, res) => {
+    res.render('alumni/overview')
+})
+app.get("/alumni/association", (req, res) => {
+    res.render('alumni/association')
+})
+app.get("/alumni/organization", (req, res) => {
+    res.render('alumni/organization')
+})
+app.get("/alumni/startups", (req, res) => {
+    res.render('alumni/startups')
+})
 
 
-app.listen(3000,(err)=>{
-    if(err){
+app.listen(3000, (err) => {
+    if (err) {
         console.log("err!!!!!");
-    }else{
+    } else {
         console.log("server started at port 3000");
     }
 })
 
-  
